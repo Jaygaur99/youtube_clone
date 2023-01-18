53bf395303edc398865e210826cb0822c2c1fa65
Jay Kumar Gaur <jaygaur99@gmail.com>
2023-01-18 22:45:25 +0530

    Implemented channel page and home page

---
+++ src/utils/fetchFromAPI.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const options = {
+  params: { maxResults: "50" },
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": import.meta.env.VITE_REACT_APP_RAPID_API_HOST,
+  },
+};
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const fetchFromAPI = async (url) => {
+  const response = await axios.get(`${BASE_URL}/${url}`, options);
+  const { data } = response;
+  return data;
+};
